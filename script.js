b8c12be1c4d63af1fd064634f632e637fb88f3e8
frank-booth <jfraley519@gmail.com>
2022-07-05 11:59:07 -0400

    began adding code

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('working')
+// variables
+let score = 0
+let scoreDisplay = document.querySelector('#score')
+let timerDisplay = document.querySelector('#timer')
+let timerCount = 10
+let moles = document.querySelectorAll('.moles')
+timerDisplay.innerHTML = timerCount
+//functions
+const startGame = () => {}
+
+const timer = setInterval(() => {
+  timerCount--
+  timerDisplay.innerHTML = timerCount
+  if (timerCount <= 0) {
+    clearInterval(timer)
+  }
+}, 1000)
+
+const moleSelection = () => {}
+
+//events
+timer()
