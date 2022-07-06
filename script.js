63b756b2df1f7667e177f1d95318a0357130c30a
frank-booth <jfraley519@gmail.com>
2022-07-06 14:41:50 -0400

    mole popping and whacking working

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ let scoreDisplay = document.querySelector('#score')
 let timerDisplay = document.querySelector('#timer')
 let timerCount = 10
 let moles = document.querySelectorAll('.moles')
+
 timerDisplay.innerHTML = timerCount
 
 //functions
-const startGame = () => {}
+const startGame = () => {
+  timerCount = 10
+  score = 0
+  timer()
+}
 
 const timer = setInterval(() => {
+  moleSelection()
   timerCount--
   timerDisplay.innerHTML = timerCount
-  moles[8].style.display = 'block'
   if (timerCount <= 0) {
     clearInterval(timer)
     timerDisplay.innerHTML = 'Game Over!'
@@ -20,8 +25,18 @@ const timer = setInterval(() => {
   }
 }, 1000)
 
+const moleReset = (num) => {
+  moles[num].style.display = 'none'
+}
+
 const moleSelection = () => {
   const moleLocal = Math.floor(Math.random() * 9)
+  moles[moleLocal].style.display = 'block'
+  moles[moleLocal].addEventListener('click', () => {
+    score++
+  })
+  scoreDisplay.innerHTML = score
 }
 
 //events
+button.addEventListener('click', startGame)
